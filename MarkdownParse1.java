e2df71597735b6b7367e461a1634ab10bd502516
murph77 <91634541+murph77@users.noreply.github.com>
2022-04-23 16:54:51 -0700

    Test

--- MarkdownParse1.java
+++ MarkdownParse1.java
@@ -5,17 +5,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParse {
+public class MarkdownParse1 {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if(!markdown.substring(currentIndex, markdown.length()).contains("[")) {
+                break; 
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(markdown.contains("!") && markdown.charAt(openBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
---
+++ MarkdownParseTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLink()throws IOException{
+        Path testFile = Path.of("./Tests/test-file8.md");
+        String result = Files.readString(testFile);
+        ArrayList<String> links = MarkdownParse1.getLinks(result);
+        List<String> output = List.of("https://something.com", "some-page.html");
+        assertEquals(output,links);
+    }
+    
+}
